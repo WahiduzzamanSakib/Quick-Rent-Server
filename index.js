4593e9745ac47c2415602a8150758d403729493d
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-07-17 03:00:59 +0600

    Booking Add

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/api/properties/booking/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await paymentCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
+
     //ownerview
     // app.get('/api/properties/booking/:ownerMail', async (req, res) => {
     //   const ownerMail = req.params.ownerMail;
